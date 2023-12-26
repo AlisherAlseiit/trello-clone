a7dc4720d11465f9d5c87c456559a5b6f96f43cf
Alish <magawin51@gmail.com>
2023-12-27 02:10:14 +0600

    added Board List, Board Page

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -68,7 +68,7 @@ export const FormPopover = ({
         </div>
         <PopoverClose ref={closeRef} asChild>
           <Button
-            className="h-auto w-ato p-2 absolute top-2 right-2 text-neutral-600"
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"
           >
             <X className="h-4 w-4"/>
