72f944a5f7925f427ef2a4e33b0d5aedff59ca31
Alish <magawin51@gmail.com>
2024-01-04 17:21:59 +0600

    fixed error

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -8,7 +8,7 @@ import { CardWithList } from "@/types";
 import { useAction } from "@/hooks/use-action";
 import { copyCard } from "@/actions/copy-card";
 import { Button } from "@/components/ui/button";
-import { deleteCard } from "@/actions/delete-list";
+import { deleteCard } from "@/actions/delete-card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useCardModal } from "@/hooks/use-card-modal";
 
